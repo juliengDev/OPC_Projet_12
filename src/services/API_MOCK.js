fff516e5464771d5c15c221106a3b832f53c783c
JulienG <juliengilbert.dev@gmail.com>
2023-09-02 06:13:34 +0200

    Refactorisation generale

--- src/services/API_MOCK.js
+++ src/services/API_MOCK.js
@@ -56,54 +56,5 @@ USER_MAIN_DATA.forEach((mainData) => {
   )
 })
 
-// const userId = USER_MAIN_DATA[0].id
-// const userInfos = USER_MAIN_DATA[0].userInfos
-// const todayScore = USER_MAIN_DATA[0].todayScore
-// const keyData = USER_MAIN_DATA[0].keyData
-// const activity = USER_ACTIVITY[0].sessions
-// const averageSessions = USER_AVERAGE_SESSIONS[0].sessions
-// const performance = USER_PERFORMANCE[0].data
-
-// users.push(
-//   new User(
-//     userId,
-//     userInfos,
-//     todayScore,
-//     keyData,
-//     activity,
-//     averageSessions,
-//     performance,
-//   ),
-// )
+console.log(users)
 export default users
-
-// In case you want to import all the entries from the database
-
-// USER_MAIN_DATA.forEach((mainData) => {
-//   const userId = mainData.id
-//   const userInfos = mainData.userInfos
-//   const todayScore = mainData.todayScore
-//   const keyData = mainData.keyData
-
-//   const activity = USER_ACTIVITY.find(
-//     (activityData) => activityData.userId === userId,
-//   )
-//   const averageSessions = USER_AVERAGE_SESSIONS.find(
-//     (sessionData) => sessionData.userId === userId,
-//   )
-//   const performance = USER_PERFORMANCE.find(
-//     (performanceData) => performanceData.userId === userId,
-//   )
-
-//   users.push(
-//     new User(
-//       userId,
-//       userInfos,
-//       todayScore,
-//       keyData,
-//       activity,
-//       averageSessions,
-//       performance,
-//     ),
-//   )
-// })
