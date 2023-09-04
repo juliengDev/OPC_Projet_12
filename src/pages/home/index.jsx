b99d2a6a719168279fc6483781d30cea228c6a11
JulienG <juliengilbert.dev@gmail.com>
2023-09-04 05:10:03 +0200

    Finalisation du projet

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import "../../styles/style.css"
-// import users from "../../services/API_MOCK"
-import users from "../../services/API"
+import users from "../../services/API_MOCK"
+// import users from "../../services/API"
 
 // components
 import Header from "../../components/header"
@@ -20,25 +20,28 @@ const Home = () => {
   const [loading, setLoading] = useState(true)
   const { id } = useParams()
 
+  // Ce useEffect s'exécute après chaque rendu initial et à chaque fois que 'datas' change.
   useEffect(() => {
-    setDatas(users)
-    setLoading(false)
+    if (users) {
+      setDatas(users)
+      setLoading(false)
+    }
   }, [])
   // console.log(datas)
 
   if (loading) {
     // Si les données sont en cours de chargement
     return (
-      <section>
-        <h1>Chargement des données en cours...</h1>
-        <h2>Veuillez patienter</h2>
+      <section className="error">
+        <h1 className="error__msg">Chargement des données en cours...</h1>
+        <h2 className="error__msg__h2">Veuillez patienter</h2>
       </section>
     )
   } else if (datas.length === 0) {
     // Si les données sont chargées mais vides
     return (
-      <section>
-        <h1>Aucune donnée disponible</h1>
+      <section className="error">
+        <h1 className="error__msg">Aucune donnée disponible</h1>
       </section>
     )
   } else {
@@ -64,6 +67,9 @@ const Home = () => {
       return (
         <section className="error">
           <h1 className="error__msg">Utilisateur non trouvé</h1>
+          <a href="/" className="reset-a">
+            Retour à la page précédente
+          </a>
         </section>
       )
     }
